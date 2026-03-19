9aa15408a433404f2bc34199f6ddb3f7bbb6fae8
Klaudia Perez <est.klaudia.perez@unimilitar.edu.co>
2026-03-18 22:57:03 -0500

    función que limpia el canvas

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d")
+
+//limpiar el canvas
+function limpiarCanvas(){
+    ctx.clearRect(0,0,canvas.clientWidth, canvas.height);
+}
