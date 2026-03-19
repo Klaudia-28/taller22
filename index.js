b9c314ba1d5a1da77a817a7c5f9f233d2f0686cf
Klaudia Perez <est.klaudia.perez@unimilitar.edu.co>
2026-03-18 22:58:12 -0500

    prueba de dibujo en el canvas

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d")
+//prueba de dibujo
+ctx.fillRect(50,50,50,50);
 
 //limpiar el canvas
 function limpiarCanvas(){
