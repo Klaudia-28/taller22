dd6307b850bc5a326f713aeb48c322028e509371
Klaudia Perez <est.klaudia.perez@unimilitar.edu.co>
2026-03-18 23:51:01 -0500

    implementación de dda para mejorar la circunferencia

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 //obtiene el canvas
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d")//dibuja en 2d
-//prueba de dibujo
-ctx.fillRect(50,50,50,50);
 
 //limpiar el canvas
 function limpiarCanvas(){
     ctx.clearRect(0,0,canvas.width, canvas.height);
 }
+//algoritmo DDA
+function dda(x1, y1, x2, y2){
+    let dx = x2 -x1;
+    let dy = y2 -y1;
+    let pasos = Math.max(Math.abs(dx), Math.abs(dy));
+    let xIncremento = dx /pasos;
+    let yIncremento = dy /pasos;
+    let x =x1;
+    let y =y1;
 
+    for(let i = 0; i <= pasos; i++){
+        ctx.fillRect(Math.round(x), Math.round(y), 1, 1);
+        x += xIncremento;
+        y += yIncremento;
+    }
+}
 //funcion para dibujar la circunferencia
 function dibujarCircunferencia(){
 //obtener valores 
@@ -17,16 +30,14 @@ const k = parseInt(document.getElementById("k").value);
 const r = parseInt(document.getElementById("r").value);
 limpiarCanvas();
 //color del dibujo
-ctx.fillStyle ="blue";
+ctx.fillStyle ="black";
 //uso de la ecuacion 
 for (let x=0;x<canvas.width;x++){
     let valor = r*r - (x - h)*(x - h);
     //validacion del valor
         if (valor >= 0) {
-
         let y = Math.sqrt(valor) + k;
         ctx.fillRect(x, y, 1, 1);
-
         let y2 = -Math.sqrt(valor) + k;
         ctx.fillRect(x, y2, 1, 1);
     }
