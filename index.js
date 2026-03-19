7af80d6d27689e42ebe0d387d482108db129d1e4
Klaudia Perez <est.klaudia.perez@unimilitar.edu.co>
2026-03-18 23:04:40 -0500

    definición de variables

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ ctx.fillRect(50,50,50,50);
 function limpiarCanvas(){
     ctx.clearRect(0,0,canvas.clientWidth, canvas.height);
 }
+
+//definir variables 
+const h = 200;//centro en x
+const k = 200;//centro en y
+const r = 200;//radio
